019e6a4ad6f9a45a8369373a2a5d82dd69747372
BrosephB <74122853+BrosephB@users.noreply.github.com>
2021-04-10 15:26:25 -0700

    Changed Mandelbrot_step.m
    changes to the fractal itself

--- Assignment3/mandelbrot_step.m
+++ Assignment3/mandelbrot_step.m
@@ -4,6 +4,6 @@ function [z,kz] = mandelbrot_step(z,kz,z0,d)
 
 %   Copyright 2014 Cleve Moler
 %   Copyright 2014 The MathWorks, Inc.
-   z = z.^2 + z0;
+   z = z.^2 + z0 * (-1)^d;
    j = (abs(z) < 2);
    kz(j) = d;
